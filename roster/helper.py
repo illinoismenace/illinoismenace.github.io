2486123d97ea9eae8c3616fff10b10db36da4664
zfrandom <zfrandomstuff@yahoo.com>
2018-11-10 09:45:48 -0600

    update captain with a star

--- roster/helper.py
+++ roster/helper.py
@@ -1,16 +1,16 @@
 HOUSE = {'Rowena Ravenclaw': 0, 'Helga Hufflepuff':1, 'Godric Gryffindor':2, 'Salazar Slytherin':3, 'Muggle':4}
 YEAR = {'Freshman':0, 'Sophomore':1, 'Junior':2, 'Senior':3, 'Graduate':4}
 import csv
-# with open('playerInfo.csv') as file:
-#     spamreader = csv.reader(file)
-#     next(spamreader)
-#     for player in spamreader:
-#         playerInfo = player
-#         house = HOUSE[playerInfo[4]]
-#         year = YEAR[playerInfo[3]]
-#         format = 'new Player("{0}", {1}, "{2}","{3}","{4}",{5}),'.format(playerInfo[1], house, playerInfo[5], playerInfo[6], playerInfo[2], year)
-#         print(format)
-#     file.close()
+with open('playerInfo.csv') as file:
+    spamreader = csv.reader(file)
+    next(spamreader)
+    for player in spamreader:
+        playerInfo = player
+        house = HOUSE[playerInfo[4]]
+        year = YEAR[playerInfo[3]]
+        format = 'new Player("{0}", {1}, "{2}","{3}","{4}",{5}),'.format(playerInfo[1], house, playerInfo[5], playerInfo[6], playerInfo[2], year)
+        print(format)
+    file.close()
 
 dummaryPlayer = [['Bonnie Wu', 'cooking fish'], ['Julia Honan', 'feeding fish'], ['Claire Szilagyi', 'eating fish'], ['Galilea Flores', 'distinguishing fish'],
  ['Mary Cook', 'catching fish'], ['Ellie Porath', 'hating fish'], ['Maura Smith', 'swimming like a fish'], ['Emma Worrell', 'fish talker']]
--- roster/javascript/roster.js
+++ roster/javascript/roster.js
@@ -4,16 +4,19 @@ function showRosterName(x){
 var HARRY_HOUSES = ['Rowena Ravenclaw', 'Helga Hufflepuff', 'Godric Gryffindor', 'Salazar Slytherin', 'Muggle']
 var YEAR = ['Freshman', 'Sophomore', 'Junior', 'Senior', 'Graduate', 'Young as Sunrise']
 class Player{
-  constructor(name, house, funFact, talent, major, year){
+  constructor(name, house, funFact, talent, major, year, isCaptain=false){
     this.name = name
     this.house = house
     this.funFact = funFact
     this.talent = talent
     this.major = major
     this.year = year
+    this.isCaptain = isCaptain
   }
   getString() {
-    return '<b>'+this.name + '</b><br>'+YEAR[this.year]+'<br>'+this.major+'<br><b>Harry Potter House<br></b>'+HARRY_HOUSES[this.house]+'<br><b>Fun Fact</b><br>'+this.funFact+'<br><b>Secret Talent</b><br>'+this.talent;
+    let name = this.name
+    if(this.isCaptain) name += '*'
+    return '<b class="roster-name">'+name + '</b><br>'+YEAR[this.year]+'<br>'+this.major+'<br><b>Harry Potter House<br></b>'+HARRY_HOUSES[this.house]+'<br><b>Fun Fact</b><br>'+this.funFact+'<br><b>Secret Talent</b><br>'+this.talent;
   }
   getImage() {
     return this.name.replace(/\s/g, '')+ '.png'
@@ -28,13 +31,25 @@ class DummyPlayer extends Player{
     return 'otter.png'
   }
 }
+
+class Coach{
+  constructor(name){
+    this.name= name
+  }
+  getImage(){
+    return 'otter.png'
+  }
+  getString(){
+    return '<b>'+this.name+'</b>'
+  }
+}
 var players = [
 new Player("Angela Gavic", 1, "I speak Bosnian.","I'm stupidly good at the Facebook Messenger game called Block Party.","Undeclared",0),
 new Player("Caroline Wheeler", 1, "I cry when I eat spicy foods","I play guitar and have double jointed ankles!","Political Science",0),
 new Player("Elie Shiro", 0, "I have 3 dogs","I can play the violin","Engineering Undeclared",0),
 new Player("Vanessa Tran", 2, "I love learning complicated yoga poses","I'm really good at sleeping. I can sleep 24hrs if given the opportunity.","Division of General Studies",0),
-new Player("Brittney Gorman", 0, "I’ve been in a movie with Nicholas Cage","Convincing Copper of anything I say","Biophysics",4),
-new Player("Cora Wessman", 0, "I stole a piece of George Washington’s House once","Art of sorts","Environmental Science",4),
+new Player("Brittney Gorman", 0, "I’ve been in a movie with Nicholas Cage","Convincing Copper of anything I say","Biophysics",4, true),
+new Player("Cora Wessman", 0, "I stole a piece of George Washington’s House once","Art of sorts","Environmental Science",4, true),
 new Player("Edith Jaucian ", 2, "I have a Russian blue cat named Bruce","I can type really fast.","Social Work ",4),
 new Player("Becca Napoli", 1, "I have double jointed thumbs","I wish","Mechanical Engineering and Spanish",2),
 new Player("Faith Zou", 3, "I really enjoy the Harry Potter series, and I cried very hard at my first HP film concert. ","I did traditional Chinese calligraphy for 6 years when I was little. ","Geographic Information Science",2),
@@ -44,7 +59,7 @@ new Player("Megan Coleman", 2, "I always have peanut butter m&m's on hand, at al
 new Player("Tanya Sinha", 0, "I have a birthmark in the shape of a goldfish","No :(","Engineering Physics",2),
 new Player("Zifan Feng", 0, "Watch all videos at 1.5x speed","Pretend that I understand everything people said","Computer Science",2),
 new Player("Zifei Feng", 2, "I didn't know I need to score in my first frisbee practice","Pretend to know nothing","Computer Science",2),
-new Player("Amy Copper", 0, "My twin sister fell down the stairs as a baby, and then I just followed her all the way down :^)","I can do that clover thing with my tongue ","Double in History and Anthropology",3),
+new Player("Amy Copper", 0, "My twin sister fell down the stairs as a baby, and then I just followed her all the way down :^)","I can do that clover thing with my tongue ","Double in History and Anthropology",3, true),
 new Player("Claire Follis", 1, "I'm from Texas","I've never lost a chapstick without finishing it first","Education Learning Science & Political Science",3),
 new Player("Lea", 3, "I’m better with animals than people","I don’t ","Animal Sciences",3),
 new Player("Cassidy Wichelecki", 0, "I've played piano since kindergarten","Im Pretty Good at claw machines ","Undeclared",1),
@@ -59,7 +74,7 @@ new DummyPlayer("Mary Cook","I eat spicy foods when I cry",0),
 new DummyPlayer("Ellie Porath", "I won the 2018 Olympic of Hula Hooping", 4),
 new DummyPlayer("Emma Worrell", "Too serious to have fun fact", 0),
 new DummyPlayer("Liza Kalinina", "I taught a 60-year-old person how to ski", 2),
-new DummyPlayer("Angelo Ramos", "", 5)
+new Coach("Angelo Ramos")
 ];
 
 var rosterSummary = document.getElementById("roster-summary")
